e243611af045c9bcf7969131fe9ef2eb417106f6
xs.zhou <newlifechou@outlook.com>
2016-04-06 14:57:55 +0800

    Add VHP Model to EF

---
+++ ReportServiceLib/Model/VHP.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    Developer:xs.zhou@outlook.com
+    CreateTime:2016/4/6 14:52:44
+*/
+namespace ReportServiceLib.Model
+{
+    public class VHP
+    {
+        public int PlanId { get; set; }
+        public DateTime VHPDate { get; set; }
+        public string VHPDevice { get; set; }
+        public string Customer { get; set; }
+        public string PO { get; set; }
+        public string ProductName { get; set; }
+        public string PMIWorkingNumber { get; set; }
+        public string Dimension { get; set; }
+    }
+}
---
+++ ReportServiceLib/VHPReport.cs
@@ -0,0 +1,21 @@
+﻿using ReportServiceLib.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    Developer:xs.zhou@outlook.com
+    CreateTime:2016/4/6 14:52:19
+*/
+namespace ReportServiceLib
+{
+    public class VHPReport
+    {
+        public List<VHP> GetVHPs()
+        {
+
+        }
+    }
+}
