57e6251c4d927e26418124eedf4289d9f099cc0a
xs.zhou <newlifechou@outlook.com>
2016-04-06 11:33:38 +0800

    create the unittest project and database has been created

---
+++ WcfReportService.UnitTest/UTProductService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WcfReportService.UnitTest
+{
+    [TestClass]
+    public class UTProductService
+    {
+        private ProductReportService service;
+
+        [TestInitialize]
+        public void Initial()
+        {
+            service = new ProductReportService();
+        }
+
+        [TestMethod]
+        public void TestGetProducts()
+        {
+            var products=service.GetProducts();
+
+            Assert.IsNotNull(products);
+        }
+    }
+}
--- WcfReportService/ProductReportService.svc.cs
+++ WcfReportService/ProductReportService.svc.cs
@@ -7,13 +7,15 @@ using System.ServiceModel.Web;
 using System.Text;
 using WcfReportService.Model;
 using ReportServiceLib;
-
+using AutoMapper;
 
 namespace WcfReportService
 {
+    
     public class ProductReportService : IProductReportService
     {
         private ReportService   service;
+
         public ProductReportService()
         {
             service = new ReportService();
@@ -31,7 +33,11 @@ namespace WcfReportService
 
         public List<Product> GetProducts()
         {
-            return service.GetProducts();
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<ReportServiceLib.Model.Product, Product>());
+            var mapper = config.CreateMapper();
+            List<Product> products=mapper.Map<List<ReportServiceLib.Model.Product>,List< Product >> (service.GetProducts());
+
+            return products;
         }
 
         public bool UpdateProduct(Product product)
