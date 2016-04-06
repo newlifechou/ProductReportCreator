492674adab123734a67d4d1fc99b699383f0c547
xs.zhou <newlifechou@outlook.com>
2016-04-06 10:38:28 +0800

    Add Report DataContract

---
+++ WcfReportService/Model/Report.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WcfReportService.Model
+{
+    [DataContract(Namespace ="Http://www.newlifechou.com")]
+    public class ProductReport
+    {
+        [DataMember]
+        public Guid Id { get; set;}
+        [DataMember]
+        public string Material { get; set; }
+        [DataMember]
+        public string Lot { get; set; }
+        [DataMember]
+        public string Size { get; set; }
+        [DataMember]
+        public string Customer { get; set; }
+        [DataMember]
+        public string PO { get; set; }
+        [DataMember]
+        public string Density { get; set; }
+        [DataMember]
+        public string Weight { get; set; }
+        [DataMember]
+        public string Resistance { get; set; }
+        [DataMember]
+        public string Composition { get; set; }
+    }
+}
