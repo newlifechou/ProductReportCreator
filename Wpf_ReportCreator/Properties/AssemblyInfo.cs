d4aa6570fae138c56d5dd632c6a25543fd26e2d9
xs.zhou <newlifechou@outlook.com>
2016-04-07 08:38:14 +0800

    organize and change the wpf project name

--- Wpf_ReportCreator/Properties/AssemblyInfo.cs
+++ Wpf_ReportCreator/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Wpf_ReportCreator")]
+[assembly: AssemblyTitle("WpfReportCreator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Wpf_ReportCreator")]
+[assembly: AssemblyProduct("WpfReportCreator")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
