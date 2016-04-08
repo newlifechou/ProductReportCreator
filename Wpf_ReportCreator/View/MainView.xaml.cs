a6dbe52523ce2c25f9a99e3e3339e19da9c0825d
xs.zhou <newlifechou@outlook.com>
2016-04-08 17:02:51 +0800

    add mainview uctargetview targeteditview

---
+++ Wpf_ReportCreator/View/MainView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfReportCreator.View
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            ucTargetView = new UCTargetView();
+            mainContent.Content = ucTargetView;
+        }
+        private UCTargetView ucTargetView;
+        
+        private void btnProductView_Click(object sender, RoutedEventArgs e)
+        {
+            mainContent.Content = ucTargetView;
+        }
+
+        private void btnSampleView_Click(object sender, RoutedEventArgs e)
+        {
+            mainContent.Content = null;
+        }
+    }
+}
---
+++ Wpf_ReportCreator/ViewModel/UCTargetViewModel.cs
@@ -0,0 +1,21 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace WpfReportCreator.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class UCTargetViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the UCTargetViewModel class.
+        /// </summary>
+        public UCTargetViewModel()
+        {
+
+        }
+    }
+}
