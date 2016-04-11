17d4ea6b0d78cec1f6c3e2cc622e0855ae57f6da
xs.zhou <newlifechou@outlook.com>
2016-04-11 09:26:59 +0800

    create ireport interface

---
+++ WpfReportCreator/Service/IReport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfReportCreator.Service
+{
+    /// <summary>
+    /// 这个接口用于生成各种格式的报告，word，xps，excel等
+    /// </summary>
+    public interface IReport
+    {
+
+    }
+}
