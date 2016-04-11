7ba619ce30515b0db105580407d0f4956ae7e16b
xs.zhou <newlifechou@outlook.com>
2016-04-11 09:25:18 +0800

    UCSampleViewModel

---
+++ WpfReportCreator/ViewModel/UCSampleViewModel.cs
@@ -0,0 +1,18 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    Developer:xs.zhou@outlook.com
+    CreateTime:2016/4/11 9:24:46
+*/
+namespace WpfReportCreator.ViewModel
+{
+    public class UCSampleViewModel:ViewModelBase
+    {
+
+    }
+}
