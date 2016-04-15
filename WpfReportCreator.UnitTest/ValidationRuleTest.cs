b6adb73178b1dc12b2028863b06f4c5eb7fe20cd
xs.zhou <newlifechou@outlook.com>
2016-04-16 05:47:14 +0800

    RequiredRule

---
+++ WpfReportCreator.UnitTest/ValidationRuleTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfReportCreator.Resource.ValidationRules;
+using System.Windows.Controls;
+
+namespace WpfReportCreator.UnitTest
+{
+    [TestClass]
+    public class ValidationRuleTest
+    {
+        [TestMethod]
+        public void TestRequiredRuleEmpty()
+        {
+            RequiredRule rule = new RequiredRule();
+            string input = "";
+            ValidationResult result = rule.Validate(input, null);
+            Assert.IsFalse(result.IsValid);
+        }
+
+        [TestMethod]
+        public void TestRequiredRuleNotEmpty()
+        {
+            RequiredRule rule = new RequiredRule();
+            string input = "hello world";
+            ValidationResult result = rule.Validate(input, null);
+            Assert.IsFalse(result.IsValid);
+        }
+    }
+}
---
+++ WpfReportCreator/Resource/ValidationRules/MustLessThan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfReportCreator.Resource.ValidationRules
+{
+    /// <summary>
+    /// 输入长度不能超过
+    /// </summary>
+    public class MustLessThan
+    {
+
+    }
+}
