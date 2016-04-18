45e34ec5583f3b03199ba6d49ade7d00f59e44b5
xs.zhou <newlifechou@outlook.com>
2016-04-18 18:03:18 +0800

    implement some parts of ucsampleeditviewmodel

--- WpfReportCreator/ViewModel/UCSampleEditViewModel.cs
+++ WpfReportCreator/ViewModel/UCSampleEditViewModel.cs
@@ -1,15 +1,48 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfReportCreator.ServiceReferenceSampleReport;
 
 namespace WpfReportCreator.ViewModel
 {
-    public class UCSampleEditViewModel:ViewModelBase
+    public class UCSampleEditViewModel : ViewModelBase
     {
+        public UCSampleEditViewModel()
+        {
+            SaveCommand = new RelayCommand(SaveExecute);
+            GiveUpCommand = new RelayCommand(GiveUpExecute);
+        }
 
+        private void GiveUpExecute()
+        {
+            throw new NotImplementedException();
+        }
 
+        private void SaveExecute()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Sample currentSample;
+        public Sample CurrentSample
+        {
+            get { return currentSample; }
+            set
+            {
+                if (value == currentSample)
+                {
+                    return;
+                }
+                currentSample = value;
+                RaisePropertyChanged(nameof(currentSample));
+            }
+        }
+
+        public RelayCommand SaveCommand { get; private set; }
+        public RelayCommand GiveUpCommand { get; private set; }
     }
 }
